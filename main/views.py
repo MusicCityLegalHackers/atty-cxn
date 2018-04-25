499a09ac25018dceb1393422ce2985b8147adf9a
Peter <lawsonpd@gmail.com>
2018-04-25 11:21:30 -0500

    Add case_lookup.html. Start case_lookup view.

--- main/views.py
+++ main/views.py
@@ -41,28 +41,30 @@ def upload_form(request):
     # probably want to check each individually since `and` short circuits
     if legal_form.is_valid() and client_form.is_valid():
       client_form_data = client_form.cleaned_data
-      # see whether client is in db already
+      # save Client to db
+      ## Should see whether client is in db already
       new_client = Client.objects.create(
         name=client_form_data['name'],
         county=client_form_data['county'],
         state=client_form_data['state'],
         email=client_form_data['email']
       )
+      # save LegalDoc to db
       new_legal_doc = LegalDoc.objects.create(
         pdf_file=request.FILES['pdf_file'],
         client=new_client
       )
       case_category = request.POST['category']
-      #TODO
-      # create Case
-      # assign Attorney to Case
+      # get next attorney
       case_attorney = assign_attorney(new_client.state, new_client.county, case_category)
+      # create Case
       new_case = Case.objects.create(
         client=new_client,
         attorney=case_attorney,
+        ## Should make sure case_id doesn't already exist in db
         case_id=gen_case_id()
       )
-      # may want to set num_days somewhere else
+      ## may want to set num_days somewhere else
       num_days = 2
       return render(
         request,
