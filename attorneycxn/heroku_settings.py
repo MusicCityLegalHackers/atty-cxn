74abf8bf8b86ade23f797d63e04030d465d97428
Peter <lawsonpd@gmail.com>
2018-04-28 22:52:57 -0500

    Edit allowed hosts.

--- attorneycxn/heroku_settings.py
+++ attorneycxn/heroku_settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     # 'localhost',
     # 'testserver',
-    'www.attorneycxn.herokuapp.com'
+    '.herokuapp.com'
 ]
 
 
@@ -170,3 +170,36 @@ django_heroku.settings(locals())
 # Connecting to postgres in Heroku
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'verbose': {
+#             'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+#                        'pathname=%(pathname)s lineno=%(lineno)s ' +
+#                        'funcname=%(funcName)s %(message)s'),
+#             'datefmt': '%Y-%m-%d %H:%M:%S'
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         }
+#     },
+#     'handlers': {
+#         'null': {
+#             'level': 'DEBUG',
+#             'class': 'logging.NullHandler',
+#         },
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'verbose'
+#         }
+#     },
+#     'loggers': {
+#         'testlogger': {
+#             'handlers': ['console'],
+#             'level': 'INFO',
+#         }
+#     }
+# }
