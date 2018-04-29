ef91d4acf98d943e7024d2cda37ce91d7abb88a4
Peter <lawsonpd@gmail.com>
2018-04-29 10:09:20 -0500

    DEBUG=False.

--- attorneycxn/heroku_settings.py
+++ attorneycxn/heroku_settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # /Users/
 #     SECRET_KEY = f.readline()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     # 'localhost',
